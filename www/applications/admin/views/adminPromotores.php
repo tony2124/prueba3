c54f65b928618d6eda1a1c50db0d7e88d9400605
Alfonso Calderon <alfonso.calderon.chavez@gmail.com>
2012-05-18 21:13:22 -0500

    Ver promotores

--- www/applications/admin/views/adminPromotores.php
+++ www/applications/admin/views/adminPromotores.php
@@ -18,7 +18,7 @@
         <td><?php print $promotor['usuario_promotor']?></td>
         <td><?php print $promotor['contrasena_promotor']?></td>
         <td><?php print $promotor['apellido_paterno_promotor'].' '.$promotor['apellido_materno_promotor'].' '.$promotor['nombre_promotor'] ?></td>
-        <td><?php print ($promotor['sexo'] == 1) : 'HOMBRE' ? 'MUJER' ?></td>
+        <td><?php print ($promotor['sexo'] == 1) ? 'HOMBRE' : 'MUJER' ?></td>
         <td><?php print $promotor['correo_electronico']?></td>
         <td><?php print edad($promotor['fecha_nacimiento']) ?></td>
         <td><?php print $promotor['ocupacion']?></td>
